e508eeafb1143d99ee30ab7ac79b96782de18fbd
Kirill-Bokov <kirill37reg2@gmail.com>
2025-02-07 19:42:28 +0300

    Improved adaptability for smartphones, added a cool scrollbar and changed the readme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,36 @@
 import { Link } from "react-router-dom";
-import '../styles/header.css'
+import "../styles/header.css";
 function Header() {
   return (
     <header className="header">
       <nav className="nav">
-        <div className="nav-left">
-          <Link to="/tsaregrad">Цареград</Link>
-          <Link to="/worldviews">Мировоззрения</Link>
-          <Link to="/important-events">Недавние события</Link>
-        </div>
-        <div className="nav-logo">
+        <div className="nav-menu">
+          <div className="nav-menu__item">
+            <Link to="/tsaregrad">Цареград</Link>
+          </div>
+          <div className="nav-menu__item">
+            <Link to="/worldviews">Мировоззрения</Link>
+          </div>
+          <div className="nav-menu__item">
+            <Link to="/important-events">Недавние события</Link>
+          </div>
+          <div className="nav-menu__item logo">
           <Link to="/">
             <img src={`${import.meta.env.BASE_URL}logo.svg`} alt="Логотип" />
           </Link>
-        </div>
-        <div className="nav-right">
-          <Link to="/countries">Страны</Link>
-          <Link to="/species">Виды</Link>
-          <Link to="/nationalities">Национальности</Link>
-          <Link to="/history">История</Link>
+          </div>
+          <div className="nav-menu__item">
+            <Link to="/countries">Страны</Link>
+          </div>
+          <div className="nav-menu__item">
+            <Link to="/species">Виды</Link>
+          </div>
+          <div className="nav-menu__item">
+            <Link to="/nationalities">Национальности</Link>
+          </div>
+          <div className="nav-menu__item">
+            <Link to="/history">История</Link>
+          </div>
         </div>
       </nav>
     </header>
